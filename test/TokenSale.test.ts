e27744550f551544353124353d5e1a7ef965e98d
IlhanYapici <ilhan-yapici@outlook.fr>
2024-01-23 12:04:15 +0100

    remove useless import

--- test/TokenSale.test.ts
+++ test/TokenSale.test.ts
@@ -1,5 +1,5 @@
 import { loadFixture } from '@nomicfoundation/hardhat-network-helpers'
-import { assert, expect } from 'chai'
+import { expect } from 'chai'
 import { ethers } from 'hardhat'
 
 describe('TokenSale', function () {
